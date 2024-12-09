f6b92591bd2adf837f3ee817135768842eccf350
rlagydlf <01021139593a@gmail.com>
2024-12-09 14:48:16 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 97.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(String[] strlist) {
+
+        int x = strlist.length;
+        int[] y = new int[x];
+        int z = 0;
+
+        for(int i = 0; i<x; i++) {
+            y[i] = strlist[i].length();
+        }
+
+        return y;
+    }
+}
