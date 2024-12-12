0e144107a74d14cd1d2ed88136b0f50e1aec81d6
rlagydlf <01021139593a@gmail.com>
2024-12-12 10:17:43 +0900

    [level 0] Title: qr code, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.js
@@ -0,0 +1,10 @@
+function solution(q, r, code) {
+    var answer = "";
+    for (var i = 0; i < code.length; i++) {
+        if (i % q == r) {
+            answer += code[i];
+        }
+    }
+   
+    return answer;
+}
